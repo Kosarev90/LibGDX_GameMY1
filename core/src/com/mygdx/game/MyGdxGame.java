8f94c275e17a7830f3485f2516fd8cef1b203c31
Kosarev90 <kosarev90sv@icloud.com>
2022-08-16 18:09:41 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,45 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Texture imgBender;
+	int clk;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		imgBender = new Texture("bender.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
+		Gdx.graphics.setTitle("Clicked " + clk + " times!");
+
+		float x = Gdx.input.getX() - imgBender.getWidth()/2;
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - imgBender.getHeight()/2;
+
+
+
+		batch.begin();
+		batch.draw(imgBender, x, y);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		imgBender.dispose();
+	}
+}
